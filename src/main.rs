ac16a8c6ec22e4242abe417b293691eb9b7b550b
Erik Nordstrøm <erik@nordstroem.no>
2018-01-11 16:01:47 +0100

    Argument handling, number generation and dice input.

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2017 Erik Nordstrøm <erik@nordstroem.no>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+use std::io::stdin;
+
+#[macro_use]
+extern crate clap;
+use clap::App;
+
+extern crate rand;
+use rand::os::OsRng;
+use rand::Rng;
+
+fn read_dice (n: u32) -> u32
+{
+    eprint!("Throw {} dice and enter the number of eyes shown on each: ", n);
+
+    let mut result = 0;
+    let mut i = 0;
+
+    while i < n
+    {
+        let mut input = String::new();
+
+        stdin().read_line(&mut input);
+
+        for c in input.chars()
+        {
+            match c
+            {
+                '1' | '2' | '3' | '4' | '5' | '6' =>
+                {
+                    result += (c.to_digit(10).unwrap() - 1) * (6 as u32).pow(i);
+                    i += 1;
+                },
+                _ => {}
+            }
+
+            if i == n
+            {
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+fn main ()
+{
+    let yaml = load_yaml!("cli.yaml");
+    let args = App::from_yaml(yaml).get_matches();
+
+    let opt_use_physical_dice = args.is_present("use_physical_dice");
+    let opt_use_short_wlist = args.is_present("use_short_wlist");
+    let opt_use_autocomplete_optimized_wlist =
+        args.is_present("use_autocomplete_optimized_wlist");
+
+    let mut num_dice: u32 = 5;
+    if opt_use_short_wlist || opt_use_autocomplete_optimized_wlist
+    {
+        num_dice = 4;
+    }
+
+    let mut num_words: usize = 6;
+    if args.is_present("num_words")
+    {
+        num_words = args.value_of("num_words").unwrap().parse::<usize>().unwrap();
+    }
+    else
+    {
+        if opt_use_short_wlist
+        {
+            num_words = 8;
+        }
+        else if opt_use_autocomplete_optimized_wlist
+        {
+            num_words = 12;
+        }
+    }
+
+    let mut word_idx = vec![0; num_words];
+
+    if opt_use_physical_dice
+    {
+        for i in 0..num_words
+        {
+            eprint!("Word {}. ", i + 1);
+            word_idx[i] = read_dice(num_dice);
+        }
+    }
+    else
+    {
+        let mut rng = OsRng::new().unwrap();
+
+        for i in 0..num_words
+        {
+            word_idx[i] = rng.gen_range(0, (6 as u32).pow(num_dice) + 1);
+        }
+    }
+
+    for i in 0..num_words
+    {
+        print!("{}", word_idx[i]);
+
+        if i < (num_words - 1)
+        {
+            print!(" ");
+        }
+        else
+        {
+            println!();
+        }
+    }
+}
