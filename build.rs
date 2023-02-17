df7dc74d3dcd66e3c7127c6fbbc0c38f4225fa2e
Erik Nordstrøm <erik@nordstroem.no>
2023-02-17 21:26:01 +0100

    cargo fmt --all

--- build.rs
+++ build.rs
@@ -5,19 +5,18 @@ use std::path::Path;
 
 // https://doc.rust-lang.org/cargo/reference/build-scripts.html#case-study-code-generation
 
-fn words (mut f_dest: &File, const_name: &str, fname_src: &str)
-{
+fn words(mut f_dest: &File, const_name: &str, fname_src: &str) {
     f_dest.write_all(b"const ").unwrap();
     f_dest.write_all(const_name.as_bytes()).unwrap();
     f_dest.write_all(b": &[&str] = &[").unwrap();
 
     let f_src = BufReader::new(File::open(fname_src).unwrap());
-    for line in f_src.lines()
-    {
+    for line in f_src.lines() {
         f_dest.write_all(b"\"").unwrap();
 
-        f_dest.write_all(line.unwrap().split('\t').nth(1).unwrap()
-            .as_bytes()).unwrap();
+        f_dest
+            .write_all(line.unwrap().split('\t').nth(1).unwrap().as_bytes())
+            .unwrap();
 
         f_dest.write_all(b"\",").unwrap();
     }
@@ -25,8 +24,7 @@ fn words (mut f_dest: &File, const_name: &str, fname_src: &str)
     f_dest.write_all(b"];").unwrap();
 }
 
-fn main ()
-{
+fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("wordlists.rs");
     let f = File::create(dest_path).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@
 use std::io;
 use std::io::{stdin, stdout, Write};
 
-use clap::load_yaml;
 use clap::crate_version;
+use clap::load_yaml;
 use clap::App;
 
 use rand::thread_rng;
@@ -27,116 +27,121 @@ use rand::Rng;
 // https://doc.rust-lang.org/cargo/reference/build-scripts.html#case-study-code-generation
 include!(concat!(env!("OUT_DIR"), "/wordlists.rs"));
 
-fn main () -> io::Result<()>
-{
-  let yaml = load_yaml!("cli.yaml");
-  let args = App::from_yaml(yaml).version(crate_version!()).get_matches();
+fn main() -> io::Result<()> {
+    let yaml = load_yaml!("cli.yaml");
+    let args = App::from_yaml(yaml).version(crate_version!()).get_matches();
 
-  let opt_use_physical_dice = args.is_present("use_physical_dice");
-  let opt_use_short_wlist = args.is_present("use_short_wlist");
-  let opt_use_long_wlist = args.is_present("use_long_wlist");
-  let opt_calculate_entropy = args.is_present("calc_entropy");
+    let opt_use_physical_dice = args.is_present("use_physical_dice");
+    let opt_use_short_wlist = args.is_present("use_short_wlist");
+    let opt_use_long_wlist = args.is_present("use_long_wlist");
+    let opt_calculate_entropy = args.is_present("calc_entropy");
 
-  let wordlist =
-  {
-    if opt_use_long_wlist       { WL_LONG }
-    else if opt_use_short_wlist { WL_SHORT }
-    else                        { WL_AUTOCOMPLETE }
-  };
-
-  let num_dice: u32 = if opt_use_long_wlist { 5 } else { 4 };
-  let wl_length = (6u32).pow(num_dice);
-
-  let num_passphrases: u32 =
-  {
-    if args.is_present("num_passphrases") { args.value_of("num_passphrases").unwrap().parse::<u32>().unwrap() }
-    else { 1 }
-  };
-
-  let num_words: usize =
-  {
-    if args.is_present("num_words") { args.value_of("num_words").unwrap().parse::<usize>().unwrap() }
-    else if opt_use_long_wlist { 10 }
-    else { 12 }
-  };
-
-  let stdout = stdout();
-  let mut handle = stdout.lock();
-
-  if opt_calculate_entropy
-  {
-    handle.write_fmt(format_args!(
-      "Current settings will create passphrases with {:.2} bits of entropy.",
-      (num_words as f64) * (wl_length as f64).log2()))?;
-  }
-  else
-  {
-    for _ in 0..num_passphrases
-    {
-      if opt_use_physical_dice
-      {
-        let mut word_idx = vec![0usize; num_words];
-
-        let width = format!("{num_words}").len();
-
-        for i in 0..num_words
-        {
-          eprint!("Word {:>w$} / {}. ", i + 1, num_words, w = width);
-          word_idx[i] = read_dice(num_dice);
+    let wordlist = {
+        if opt_use_long_wlist {
+            WL_LONG
+        } else if opt_use_short_wlist {
+            WL_SHORT
+        } else {
+            WL_AUTOCOMPLETE
         }
+    };
 
-        for i in 0..num_words
-        {
-          handle.write_all(wordlist[word_idx[i]].as_bytes())?;
-          if i < (num_words - 1) { handle.write_all(b" ")?; }
+    let num_dice: u32 = if opt_use_long_wlist { 5 } else { 4 };
+    let wl_length = (6u32).pow(num_dice);
+
+    let num_passphrases: u32 = {
+        if args.is_present("num_passphrases") {
+            args.value_of("num_passphrases")
+                .unwrap()
+                .parse::<u32>()
+                .unwrap()
+        } else {
+            1
         }
-      }
-      else
-      {
-        let mut rng = thread_rng();
+    };
 
-        for i in 0..num_words
-        {
-          handle.write_all(wordlist[rng.gen_range(0, wl_length) as usize].as_bytes())?;
-          if i < (num_words - 1) { handle.write_all(b" ")?; }
+    let num_words: usize = {
+        if args.is_present("num_words") {
+            args.value_of("num_words")
+                .unwrap()
+                .parse::<usize>()
+                .unwrap()
+        } else if opt_use_long_wlist {
+            10
+        } else {
+            12
         }
-      }
+    };
 
-      handle.write_all(b"\n")?;
+    let stdout = stdout();
+    let mut handle = stdout.lock();
+
+    if opt_calculate_entropy {
+        handle.write_fmt(format_args!(
+            "Current settings will create passphrases with {:.2} bits of entropy.",
+            (num_words as f64) * (wl_length as f64).log2()
+        ))?;
+    } else {
+        for _ in 0..num_passphrases {
+            if opt_use_physical_dice {
+                let mut word_idx = vec![0usize; num_words];
+
+                let width = format!("{num_words}").len();
+
+                for i in 0..num_words {
+                    eprint!("Word {:>w$} / {}. ", i + 1, num_words, w = width);
+                    word_idx[i] = read_dice(num_dice);
+                }
+
+                for i in 0..num_words {
+                    handle.write_all(wordlist[word_idx[i]].as_bytes())?;
+                    if i < (num_words - 1) {
+                        handle.write_all(b" ")?;
+                    }
+                }
+            } else {
+                let mut rng = thread_rng();
+
+                for i in 0..num_words {
+                    handle.write_all(wordlist[rng.gen_range(0, wl_length) as usize].as_bytes())?;
+                    if i < (num_words - 1) {
+                        handle.write_all(b" ")?;
+                    }
+                }
+            }
+
+            handle.write_all(b"\n")?;
+        }
     }
-  }
 
-  Ok(())
+    Ok(())
 }
 
-fn read_dice (n: u32) -> usize
-{
-  eprint!("Throw {n} dice and enter the number of eyes shown on each: ");
+fn read_dice(n: u32) -> usize {
+    eprint!("Throw {n} dice and enter the number of eyes shown on each: ");
 
-  let mut result = 0;
-  let mut i = 0;
+    let mut result = 0;
+    let mut i = 0;
 
-  while i < n
-  {
-    let mut input = String::new();
+    while i < n {
+        let mut input = String::new();
 
-    stdin().read_line(&mut input).unwrap();
+        stdin().read_line(&mut input).unwrap();
 
-    for c in input.chars()
-    {
-      match c
-      {
-        '1' | '2' | '3' | '4' | '5' | '6' =>
-        {
-          result += (c.to_digit(10).unwrap() - 1) * (6u32).pow(n - i - 1);
-          i += 1;
-        },
-        _ => {},
-      }
+        for c in input.chars() {
+            match c {
+                '1' | '2' | '3' | '4' | '5' | '6' => {
+                    result += (c.to_digit(10).unwrap() - 1) * (6u32).pow(n - i - 1);
+                    i += 1;
+                }
+                _ => {}
+            }
 
-      if i == n { break; }
+            if i == n {
+                break;
+            }
+        }
     }
-  }
 
-  result as usize
+    result as usize
 }
